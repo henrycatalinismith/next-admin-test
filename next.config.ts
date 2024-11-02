9d80a396b7f72b222d3964b899d5f4eb6bb65432
Henry Catalini Smith <henry@catalinismith.com>
2024-11-02 15:53:44 +0100

    fix base path

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/next-admin-test',
 };
 
 export default nextConfig;
